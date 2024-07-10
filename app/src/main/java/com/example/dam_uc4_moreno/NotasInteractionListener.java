3eb6455faa8a226c45d10e57583c042ccdaa403e
Facundo Moreno <1913100113@untels.edu.pe>
2024-07-10 11:01:35 -0500

    Primer commit

---
+++ app/src/main/java/com/example/dam_uc4_moreno/NotasInteractionListener.java
@@ -0,0 +1,8 @@
+package com.example.dam_uc4_moreno;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+
+}
